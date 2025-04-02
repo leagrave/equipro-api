0a1c6a1c267fe5bd0003efd630a8761d8f9618e0
leagrave <lea.grave@orange.fr>
2025-04-02 19:27:31 +0200

    Modification du routes users suite

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../config/db');
 
 // Créer un nouvel utilisateur
 router.post('/', async (req, res) => {
